c953a9c1e3829412ccc8532cc4f364c4697ec343
sanjeeb_09 <sanjeeb776@gmail.com>
2024-03-13 14:18:15 +0530

    add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -22,6 +23,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');

3265f99f443c9f0dfc5f63ba9fefeae713a07975
sanjeeb_09 <sanjeeb776@gmail.com>
2024-03-14 07:50:35 +0530

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Unity Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to Unity Blog! This blog was created by Sanjeeb Kumar Sahoo
+              as a personal project to share his thoughts and ideas with the
+              world. Sanjeeb is a passionate civil aspirant who loves to write about
+              both India as well as World Affairs. He is currently pursuing Msc. Data Science.
+            </p>
+
+            <p>
+              On this blog, you will find daily articles and news on topics
+              Polity & Governance, Science & Tech, Geography & Environment, Economy, International Relations and Internal Security.
+            </p>
+
+            <p>
+              We encourage you to leave comments on our posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. We believe that a community of learners can help
+              each other grow and improve.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction';
 
 export default function Projects() {
   return (
-    <div>
-      Projects
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Pojects</h1>
+      <p className='text-md text-gray-500'>Visit our repository to build fun and engaging projects with HTML, CSS, and JavaScript!</p>
+      <CallToAction />
     </div>
   )
 }

a4477c5f16fdc2fbe5944a5b288bab69780a5866
sanjeeb_09 <sanjeeb776@gmail.com>
2024-03-14 07:57:30 +0530

    Pre installation

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
-import postRoutes from './routes/post.route.js';
-import commentRoutes from './routes/comment.route.js';
-import cookieParser from 'cookie-parser';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -18,27 +19,34 @@ mongoose
     console.log(err);
   });
 
-const app = express(); 
+const __dirname = path.resolve();
+
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/post', postRoutes);
-app.use('/api/comment', commentRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000!');
+  console.log("Server is running on port 3000!");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
   });
-  
-  app.use('/api/user', userRoutes);
-  app.use('/api/auth', authRoutes);
-  
-  app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
-  });
+});
